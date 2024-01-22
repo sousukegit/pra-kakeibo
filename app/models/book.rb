4fee8fdc287652cf2578df470f01f880582cc74b
sousukegit <greenneosoul.sousuke@gmail.com>
2024-01-22 15:05:43 +0900

    家計簿アプリ完了

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+    #書き方：validates :カラム名　ルール
+    ##presenseは入力必須にする
+    validates:year,presence:true
+    validates:month,presence:true
+    validates:inout,presence:true
+    validates:category,presence:true
+    validates:amount,presence:true
+
 end
